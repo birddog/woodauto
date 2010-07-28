b139b2e3b7c86e5c8eb718dfa9134e36a3ed2753
Chad Payne <chad@bird-dog.ca>
2010-07-28 16:43:59 -0600

    Google maps script

--- custom.js
+++ custom.js
@@ -459,7 +459,9 @@ function googlemap() {
 						position: new google.maps.LatLng(lat, lng),
 						map: map
 					});
-					
+					google.maps.event.addDomListener(document.getElementById("linkID"), "click", function(ev) {
+						map.setCenter(marker.getPosition());
+					});	
 				
 					// Create listener for click event to open info window
 					google.maps.event.addListener(marker, 'click', (function(marker, i) {
